349d1bcb1dc26e0a08bbcb6595b3bc69d53dc0c2
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2019-03-10 19:11:54 -0400

    almost there

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('The Carolina Burger');
+INSERT INTO burgers (burger_name) VALUES ('Raleigh Times Burger');
+INSERT INTO burgers (burger_name) VALUES ('Same I am Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Hall of Fame Burger');
+
